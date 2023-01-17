896036f4b915e11a44e98f9d0740c1495b9cd91e
bianca <me@biancarosa.com.br>
2023-01-17 17:09:50 -0300

    logs: print host we are connecting to

--- database/client.go
+++ database/client.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -12,11 +13,13 @@ import (
 
 func GetClient() (*mongo.Client, context.Context, context.CancelFunc) {
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	host := os.Getenv("MONGO_HOST")
+	log.Println("Connecting on host", host)
 	dbUri := fmt.Sprintf(
 		"mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority&ssl=true",
 		os.Getenv("MONGO_USERNAME"),
 		os.Getenv("MONGO_PASSWORD"),
-		os.Getenv("MONGO_HOST"),
+		host,
 	)
 	clientOptions := options.Client().
 		ApplyURI(dbUri).
